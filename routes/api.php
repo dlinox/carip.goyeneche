e81598458dfee4837acfb4dbb6cda6cc5fc21d13
lino <nearlino20@gmail.com>
2023-12-31 12:03:53 -0500

    dd

--- routes/api.php
+++ routes/api.php
@@ -94,7 +94,7 @@ Route::get('/convocatorias', [\App\Http\Controllers\Api\ApiController::class, 'c
 //publicaciones
 Route::get('/publicaciones', [\App\Http\Controllers\Api\ApiController::class, 'publicaciones']);
 //publicaciones by slug
-Route::get('/   /{slug}', [\App\Http\Controllers\Api\ApiController::class, 'publicacionesBySlug']);
+Route::get('/publicaciones/{slug}', [\App\Http\Controllers\Api\ApiController::class, 'publicacionesBySlug']);
 //getPublicacionesByAuthorArea
 Route::get('/publicaciones/author-area/{id}', [\App\Http\Controllers\Api\ApiController::class, 'getPublicacionesByAuthorArea']);
 
